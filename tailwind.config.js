4717208d6b49704b708a03c4bf479e32760f0c5c
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-05-17 00:58:03 -0300

    feat: implemented home and contact

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    dropShadow: {
+      'menu': '0 8px 8px rgba(0, 0, 0, 0.36)',
+    }
+  },
+  plugins: [],
+}
